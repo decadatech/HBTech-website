c8bc0e69c22dcea23b3c2c10422bde12dff8fe00
Gabriel Souza <gabrielspereira2604@gmail.com>
2020-03-10 17:52:57 -0300

    Inserção do logotipo

--- layout/scripts/indexFunctions.js
+++ layout/scripts/indexFunctions.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
     // FORMATAÇÃO DO CAMPO PHONE
-    $('#phone').mask('(00) 0 0000-0000');
+    $('#phone').mask('(00) 0000-00009');
     $('#phone').blur(function(event) {
         if($(this).val().length == 15){ // Celular com 9 dígitos + 2 dígitos DDD e 4 da máscara
-            $('#phone').mask('(00) 0 0000-0000');
+           $('#phone').mask('(00) 00000-0009');
         } else {
-            $('#phone').mask('(00) 0 0000-0000');
+           $('#phone').mask('(00) 0000-00009');
         }
-    });  
+     });
 
     jQuery("#backtotop").click(function () {
         jQuery("body,html").animate({
@@ -52,5 +52,5 @@ $(document).ready(function() {
             scrollTop: $('#contato').offset().top
         }, 1000);
     });
-    
+
 });
